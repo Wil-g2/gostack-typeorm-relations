bfd7861dcb906ea3b41151bae87717c4839862fc
Wil-g2 <wil-g2@hotmail.com>
2020-08-06 22:02:09 -0300

    feat: added Services

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -12,10 +12,25 @@ interface IRequest {
 
 @injectable()
 class CreateCustomerService {
-  constructor(private customersRepository: ICustomersRepository) {}
+  constructor(
+    @inject('CustomersRepository')
+    private customersRepository: ICustomersRepository,
+  ) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
     // TODO
+    const customers = await this.customersRepository.findByEmail(email);
+
+    if (customers) {
+      throw new AppError('Email is already exists');
+    }
+
+    const customer = await this.customersRepository.create({
+      email,
+      name,
+    });
+
+    return customer;
   }
 }
 
--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,50 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
     // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('Customer not found');
+    }
+
+    const productsIds = products.map(product => {
+      return { id: product.id };
+    });
+
+    const productsById = await this.productsRepository.findAllById(productsIds);
+
+    const productsFormat = productsById.map(product => {
+      const productFormat = products.find(
+        productFind => productFind.id === product.id,
+      );
+
+      return {
+        product_id: product.id,
+        price: product.price,
+        quantity: productFormat?.quantity || 0,
+      };
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: productsFormat,
+    });
+
+    return order;
   }
 }
 
